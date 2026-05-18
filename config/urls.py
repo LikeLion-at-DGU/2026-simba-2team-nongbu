881feb8ee713b4d587a059ff03b1096a0c488fdd
onlyvalue <sarahkim2917@gmail.com>
2026-05-19 02:41:48 +0900

    init: django 프로젝트 초기 세팅

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for mtv project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import mainpage
+from app.views import mainpage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
