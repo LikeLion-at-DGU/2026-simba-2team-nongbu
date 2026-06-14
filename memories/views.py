59a4de0f312a72df9b72a6475ad3593069112352
khxxn <kyohyun58@naver.com>
2026-06-14 16:05:00 +0900

    Feat: "추가 추억탭에 기간 만료된 우주만 출력"
    - memory_main 수정
    - test를 위한 파일이 깃에 올라가지 않도록 .gitignore 수정

--- memories/views.py
+++ memories/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render, redirect
 from spaces.models import Space
+from django.utils import timezone
+from datetime import timedelta
 
 def memory_main(request):
     if not request.user.is_authenticated:
         return redirect('accounts:login')
     
-    my_memories = Space.objects.filter(members__user=request.user)
-    return render(request, 'memory/memory_main.html', {'memories': my_memories})
+    spaces = Space.objects.filter(members__user=request.user)
+    
+    memories = []
+
+    for space in spaces:
+        end_date = space.created_at + timedelta(days=space.duration_days)
+        if timezone.now() >= end_date:
+            memories.append(space)
+
+    print("\n" + "="*40)
+    print(f"현재 로그인한 유저: {request.user}")
+    print(f"추억에 담긴 우주 개수: {len(memories)}개")
+    
+    for memory in memories:
+        print(f"방 이름: {memory.name}")
+    print("="*40 + "\n")
+
+
+    return render(request, 'memory/memory_main.html', {'memories': memories})
